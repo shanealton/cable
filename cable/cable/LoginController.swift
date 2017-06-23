0f5c2a88f1b33fd96925667337e507f3f64f0f59
Shane Alton <shanealton1@gmail.com>
2017-06-23 15:16:53 -0400

    Add header and login sections
    Added header and login section containers to LoginController

--- cable/cable/LoginController.swift
+++ cable/cable/LoginController.swift
@@ -10,9 +10,85 @@ import UIKit
 
 class LoginController: UIViewController {
   
+  // Header section
+  
+  let headerContainer: UIView = {
+    let view = UIView()
+    view.backgroundColor = .white
+    view.translatesAutoresizingMaskIntoConstraints = false
+    return view
+  }()
+  
+  let headerSeparator: UIView = {
+    let view = UIView()
+    view.backgroundColor = .rgb(red: 235, green: 232, blue: 228)
+    view.translatesAutoresizingMaskIntoConstraints = false
+    return view
+  }()
+  
+  // Login/Register section
+  
+  let inputsContainerView: UIView = {
+    let view = UIView()
+    view.backgroundColor = .lightGray
+    view.translatesAutoresizingMaskIntoConstraints = false
+    return view
+  }()
+  
+  let registerButton: UIButton = {
+    let button = UIButton(type: .system)
+    button.backgroundColor = .rgb(red: 90, green: 97, blue: 117)
+    button.layer.cornerRadius = 2
+    button.setTitle("Register", for: .normal)
+    button.tintColor = .white
+    button.translatesAutoresizingMaskIntoConstraints = false
+    return button
+  }()
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    
     view.backgroundColor = .white
+    
+    view.addSubview(headerContainer)
+    view.addSubview(headerSeparator)
+    
+    view.addSubview(inputsContainerView)
+    view.addSubview(registerButton)
+    
+    setupHeaderContainer()
+    setupHeaderSeparator()
+    
+    setupInputs()
+    setupRegisterButton()
+  }
+  
+  // Header section
+  
+  func setupHeaderContainer() {
+    headerContainer.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+    headerContainer.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+    headerContainer.heightAnchor.constraint(equalToConstant: 58).isActive = true
+  }
+  
+  func setupHeaderSeparator() {
+    headerSeparator.bottomAnchor.constraint(equalTo: headerContainer.bottomAnchor).isActive = true
+    headerSeparator.widthAnchor.constraint(equalTo: headerContainer.widthAnchor).isActive = true
+    headerSeparator.heightAnchor.constraint(equalToConstant: 0.75).isActive = true
+  }
+  
+  // Login/Register section
+  
+  func setupInputs() {
+    inputsContainerView.topAnchor.constraint(equalTo: headerContainer.bottomAnchor, constant: 16).isActive = true
+    inputsContainerView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+    inputsContainerView.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -32).isActive = true
+    inputsContainerView.heightAnchor.constraint(equalToConstant: 150).isActive = true
+  }
+  
+  func setupRegisterButton() {
+    registerButton.topAnchor.constraint(equalTo: inputsContainerView.bottomAnchor, constant: 16).isActive = true
+    registerButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+    registerButton.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -32).isActive = true
+    registerButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
   }
 }

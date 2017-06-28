f693f41aec4d19df6c6c1cd340604b33a7035e65
Shane Alton <shanealton1@gmail.com>
2017-06-27 20:19:51 -0400

    Add LoginController handlers extension
    Moved all of the login controller's handler functions to LoginController+handlers file.

---
+++ cable/cable/LoginController+handlers.swift
@@ -0,0 +1,97 @@
+//
+//  LoginController+handlers.swift
+//  cable
+//
+//  Created by Shane Alton on 6/27/17.
+//  Copyright © 2017 Shane Alton. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+extension LoginController {
+  
+  // Segmented Control Sections
+  
+  func handleControlChange() {
+    let title = segmentedControl.titleForSegment(at: segmentedControl.selectedSegmentIndex)
+    registerButton.setTitle(title, for: .normal)
+    
+    inputsContainerViewHeightAnchor?.constant = segmentedControl.selectedSegmentIndex == 0 ? 100 : 150
+    
+    nameTextFieldHeightAnchor?.isActive = false
+    nameTextFieldHeightAnchor = nameTextField.heightAnchor.constraint(equalTo: inputsContainerView.heightAnchor, multiplier: segmentedControl.selectedSegmentIndex == 0 ? 0 : 1/3)
+    nameTextFieldHeightAnchor?.isActive = true
+    
+    nameSeparator.isHidden = segmentedControl.selectedSegmentIndex == 0 ? true : false
+    
+    emailTextFieldHeightAnchor?.isActive = false
+    emailTextFieldHeightAnchor = emailTextField.heightAnchor.constraint(equalTo: inputsContainerView.heightAnchor, multiplier: segmentedControl.selectedSegmentIndex == 0 ? 1/2 : 1/3)
+    emailTextFieldHeightAnchor?.isActive = true
+    
+    passwordTextFieldHeightAnchor?.isActive = false
+    passwordTextFieldHeightAnchor = passwordTextField.heightAnchor.constraint(equalTo: inputsContainerView.heightAnchor, multiplier: segmentedControl.selectedSegmentIndex == 0 ? 1/2 : 1/3)
+    passwordTextFieldHeightAnchor?.isActive = true
+  }
+  
+  // Login/Register handlers
+  
+  func handleLoginRegister() {
+    if segmentedControl.selectedSegmentIndex == 0 {
+      handleLogin()
+    } else {
+      handleRegister()
+    }
+  }
+  
+  func handleLogin() {
+    guard let email = emailTextField.text, let password = passwordTextField.text else {
+      print("Form is not valid. Please enter your email and password.")
+      return
+    }
+    FIRAuth.auth()?.signIn(withEmail: email, password: password, completion: { (user, err) in
+      if err != nil {
+        print(err ?? "Error:")
+        return
+      }
+      print("User Login successful")
+      
+      // Add a pause to prevent visibility of a previously logged in users messages and info.
+      let when = DispatchTime.now() + 1
+      DispatchQueue.main.asyncAfter(deadline: when) {
+        self.dismiss(animated: true, completion: nil)
+      }
+    })
+  }
+  
+  func handleRegister() {
+    guard let email = emailTextField.text, let password = passwordTextField.text, let name = nameTextField.text else {
+      print("invalid email or password")
+      return
+    }
+    FIRAuth.auth()?.createUser(withEmail: email, password: password, completion: {(user: FIRUser?, error) in
+      if error != nil {
+        print(error ?? "Error")
+        return
+      }
+      
+      // reference by user id:
+      guard let uid = user?.uid else { return }
+      
+      //successfully authenticated user
+      let ref = FIRDatabase.database().reference(fromURL: "https://cable-610b1.firebaseio.com/")
+      let usersReference = ref.child("users").child(uid)
+      let values = ["name": name, "email": email]
+      usersReference.updateChildValues(values, withCompletionBlock: { (err, ref) in
+        if err != nil {
+          print(err ?? "Error")
+          return
+        }
+        print("Successfully saved new user into Firebase DB.")
+        self.dismiss(animated: true, completion: nil)
+      })
+      
+      print("Authenticated User")
+    })
+  }
+}
--- cable/cable/LoginController.swift
+++ cable/cable/LoginController.swift
@@ -11,6 +11,13 @@ import Firebase
 
 class LoginController: UIViewController {
   
+  // Dynamic height constraints for login/register fields.
+  
+  var inputsContainerViewHeightAnchor: NSLayoutConstraint?,
+      nameTextFieldHeightAnchor:       NSLayoutConstraint?,
+      emailTextFieldHeightAnchor:      NSLayoutConstraint?,
+      passwordTextFieldHeightAnchor:   NSLayoutConstraint?
+  
   // Header section
   
   let headerContainer: UIView = {
@@ -29,31 +36,6 @@ class LoginController: UIViewController {
     control.translatesAutoresizingMaskIntoConstraints = false
     return control
   }()
-  
-  func handleControlChange() {
-    let title = segmentedControl.titleForSegment(at: segmentedControl.selectedSegmentIndex)
-    registerButton.setTitle(title, for: .normal)
-    
-    // Dynamic height of inputs container
-    inputsContainerViewHeightAnchor?.constant = segmentedControl.selectedSegmentIndex == 0 ? 100 : 150
-    
-    // Dynamic height for name field
-    nameTextFieldHeightAnchor?.isActive = false
-    nameTextFieldHeightAnchor = nameTextField.heightAnchor.constraint(equalTo: inputsContainerView.heightAnchor, multiplier: segmentedControl.selectedSegmentIndex == 0 ? 0 : 1/3)
-    nameTextFieldHeightAnchor?.isActive = true
-    
-    nameSeparator.isHidden = segmentedControl.selectedSegmentIndex == 0 ? true : false
-    
-    // Dynamic height for email field
-    emailTextFieldHeightAnchor?.isActive = false
-    emailTextFieldHeightAnchor = emailTextField.heightAnchor.constraint(equalTo: inputsContainerView.heightAnchor, multiplier: segmentedControl.selectedSegmentIndex == 0 ? 1/2 : 1/3)
-    emailTextFieldHeightAnchor?.isActive = true
-    
-    // Dynamic height for password field
-    passwordTextFieldHeightAnchor?.isActive = false
-    passwordTextFieldHeightAnchor = passwordTextField.heightAnchor.constraint(equalTo: inputsContainerView.heightAnchor, multiplier: segmentedControl.selectedSegmentIndex == 0 ? 1/2 : 1/3)
-    passwordTextFieldHeightAnchor?.isActive = true
-  }
   
   let headerSeparator: UIView = {
     let view = UIView()
@@ -137,65 +119,6 @@ class LoginController: UIViewController {
     return button
   }()
   
-  func handleLoginRegister() {
-    if segmentedControl.selectedSegmentIndex == 0 {
-      handleLogin()
-    } else {
-      handleRegister()
-    }
-  }
-  
-  func handleLogin() {
-    guard let email = emailTextField.text, let password = passwordTextField.text else {
-      print("Form is not valid. Please enter your email and password.")
-      return
-    }
-    FIRAuth.auth()?.signIn(withEmail: email, password: password, completion: { (user, err) in
-      if err != nil {
-        print(err ?? "Error:")
-        return
-      }
-      print("User Login successful")
-      
-      // Add a pause to prevent visibility of a previously logged in users messages and info.
-      let when = DispatchTime.now() + 1
-      DispatchQueue.main.asyncAfter(deadline: when) {
-        self.dismiss(animated: true, completion: nil)
-      }
-    })
-  }
-  
-  func handleRegister() {
-    guard let email = emailTextField.text, let password = passwordTextField.text, let name = nameTextField.text else {
-      print("invalid email or password")
-      return
-    }
-    FIRAuth.auth()?.createUser(withEmail: email, password: password, completion: {(user: FIRUser?, error) in
-      if error != nil {
-        print(error ?? "Error")
-        return
-      }
-      
-      // reference by user id:
-      guard let uid = user?.uid else { return }
-      
-      //successfully authenticated user
-      let ref = FIRDatabase.database().reference(fromURL: "https://cable-610b1.firebaseio.com/")
-      let usersReference = ref.child("users").child(uid)
-      let values = ["name": name, "email": email]
-      usersReference.updateChildValues(values, withCompletionBlock: { (err, ref) in
-        if err != nil {
-          print(err ?? "Error")
-          return
-        }
-        print("Successfully saved new user into Firebase DB.")
-        self.dismiss(animated: true, completion: nil)
-      })
-      
-      print("Authenticated User")
-    })
-  }
-  
   override func viewDidLoad() {
     super.viewDidLoad()
     view.backgroundColor = .white
@@ -210,13 +133,6 @@ class LoginController: UIViewController {
     setupInputsContainer()
     setupRegisterButton()
   }
-  
-  // Dynamic height constraints for login/register fields.
-  
-  var inputsContainerViewHeightAnchor: NSLayoutConstraint?,
-      nameTextFieldHeightAnchor:       NSLayoutConstraint?,
-      emailTextFieldHeightAnchor:      NSLayoutConstraint?,
-      passwordTextFieldHeightAnchor:   NSLayoutConstraint?
   
   // Header section
   

c3da77255be4847bdcbe15f63971dc3d2d0a3929
Shane Alton <shanealton1@gmail.com>
2017-06-29 10:49:35 -0400

    Add HomeController handlers file
    Moved the handler functions from HomeController into +handlers file.

---
+++ cable/cable/HomeController+handlers.swift
@@ -0,0 +1,51 @@
+//
+//  HomeController+handlers.swift
+//  cable
+//
+//  Created by Shane Alton on 6/28/17.
+//  Copyright © 2017 Shane Alton. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+
+extension HomeController {
+  
+  // Check if user is currently logged in.
+  func handleAuth() {
+    if FIRAuth.auth()?.currentUser?.uid == nil {
+      perform(#selector(handleLogout), with: nil, afterDelay: 0)
+    } else {
+      guard let uid = FIRAuth.auth()?.currentUser?.uid else { return }
+      FIRDatabase.database().reference().child("users").child(uid).observeSingleEvent(of: .value, with: { (snapshot) in
+        print(snapshot)
+        if let dictionary = snapshot.value as? [String: AnyObject] {
+          self.navigationItem.title = dictionary["name"] as? String
+        }
+      })
+    }
+  }
+  
+  // Logout the current user.
+  func handleLogout() {
+    do { try FIRAuth.auth()?.signOut() } catch let err {
+      print(err)
+    }
+    
+    let loginController = LoginController()
+    present(loginController, animated: false, completion: nil)
+  }
+  
+  // Present the new message controller.
+  func handleNewMessage() {
+    let newMessageController = NewMessageController(collectionViewLayout: UICollectionViewFlowLayout())
+    present(UINavigationController(rootViewController: newMessageController), animated: true, completion: nil)
+  }
+  
+  // Present the account settings controller.
+  func handleAccount() {
+    let accountController = AccountController(collectionViewLayout: UICollectionViewFlowLayout())
+    present(UINavigationController(rootViewController: accountController), animated: true, completion: nil)
+  }
+}
--- cable/cable/HomeController.swift
+++ cable/cable/HomeController.swift
@@ -25,53 +25,17 @@ class HomeController: UICollectionViewController, UICollectionViewDelegateFlowLa
     handleAuth()
   }
   
-  // Check if user is currently logged in.
-  func handleAuth() {
-    if FIRAuth.auth()?.currentUser?.uid == nil {
-      perform(#selector(handleLogout), with: nil, afterDelay: 0)
-    } else {
-      guard let uid = FIRAuth.auth()?.currentUser?.uid else { return }
-      FIRDatabase.database().reference().child("users").child(uid).observeSingleEvent(of: .value, with: { (snapshot) in
-        print(snapshot)
-        if let dictionary = snapshot.value as? [String: AnyObject] {
-          self.navigationItem.title = dictionary["name"] as? String
-        }
-      })
-    }
-  }
-  
   fileprivate func setupNavBar() {
     self.navigationController?.navigationBar.tintColor = UIColor.rgb(red: 130, green: 122, blue: 210)
     navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Logout", style: .plain, target: self, action: #selector(handleLogout))
-    
     let account = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(handleAccount))
     let compose = UIBarButtonItem(barButtonSystemItem: .compose, target: self, action: #selector(handleNewMessage))
-    
     navigationItem.rightBarButtonItems = [account, compose]
-  }
-  
-  func handleAccount() {
-    let accountController = AccountController(collectionViewLayout: UICollectionViewFlowLayout())
-    present(UINavigationController(rootViewController: accountController), animated: true, completion: nil)
   }
   
   fileprivate func setupCollectionView() {
     self.collectionView?.register(HomeCell.self, forCellWithReuseIdentifier: "cellId")
     collectionView?.backgroundColor = .white
-  }
-  
-  func handleLogout() {
-    do { try FIRAuth.auth()?.signOut() } catch let err {
-      print(err)
-    }
-    
-    let loginController = LoginController()
-    present(loginController, animated: false, completion: nil)
-  }
-  
-  func handleNewMessage() {
-    let newMessageController = NewMessageController(collectionViewLayout: UICollectionViewFlowLayout())
-    present(UINavigationController(rootViewController: newMessageController), animated: true, completion: nil)
   }
   
   override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
